7e4c030e774b97b35ebe83dd822cc4092002b074
judejay <judejay26@gmail.com>
2023-07-25 17:33:52 +0100

    item

--- components/MealItem.tsx
+++ components/MealItem.tsx
@@ -12,19 +12,19 @@ type MealItemProps ={
 const MealItem: React.FC<MealItemProps> = ({title, imageUrl, duration, complexity, affordability}) => {
     return <View style={styles.mealItem}>  
         <Pressable 
-         android_ripple={{color: '#ccc'}} style={({pressed}) => [styles.button, pressed ? styles.button: null, ]
+         android_ripple={{color: '#ccc'}} style={({pressed}) =>  pressed ? styles.buttonPressed: null
 } >
-            <View style={styles.innerContainer}>
-            <View>
-                <Image source={{uri: imageUrl}} style={styles.image} />
-            <Text style={styles.title}>{title}</Text> 
-            </View>
-            <View style={styles.details} >
-                <Text style={styles.detailItem}> {duration}m</Text>
-                <Text style={styles.detailItem}> {complexity.toUpperCase()} </Text>
-                <Text style={styles.detailItem}> {affordability.toUpperCase()} </Text> 
-            </View>
-       </View>
+          <View style={styles.innerContainer}>
+                <View>
+                   <Image source={{uri: imageUrl}} style={styles.image} />
+                   <Text style={styles.title}>{title}</Text> 
+                </View>
+                <View style={styles.details} >
+                   <Text style={styles.detailItem}> {duration}m</Text>
+                   <Text style={styles.detailItem}> {complexity.toUpperCase()} </Text>
+                   <Text style={styles.detailItem}> {affordability.toUpperCase()} </Text> 
+                </View>
+         </View>
         </Pressable>
     </View>
 };
